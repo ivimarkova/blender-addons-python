bb3804f26320c559f65aedeb1efc99b90bc37096
Ивайла Маркова (Ivayla Markova) <154016464+ivimarkova@users.noreply.github.com>
2023-12-17 01:02:52 +0200

    Add files via upload
    open the files throught Blender or Python IDLE (3.10.10)

---
+++ AddObjectScript.py
@@ -0,0 +1,31 @@
+import bpy
+
+class TestPanel(bpy.types.Panel):
+    bl_label="Test Panel"
+    bl_idname="PT_TestPanel"
+    bl_space_type='VIEW_3D'
+    bl_region_type='UI'
+    bl_category='My 1st Addon'
+    
+    def draw(self,context):
+        layout=self.layout
+        
+        row=layout.row()
+        row.label(text="Add an object", icon='CUBE')
+        row=layout.row()
+        row.operator("mesh.primitive_cube_add", icon='CUBE')
+        row=layout.row()
+        row.operator("mesh.primitive_uv_sphere_add", icon='SPHERE')
+        row=layout.row()
+        row.operator("object.text_add", text="Font Button")
+        
+        
+        
+def register():
+    bpy.utils.register_class(TestPanel)
+    
+def unregister():
+    bpy.utils.unregister_class(TestPanel)
+    
+if __name__=="__main__":
+    register()
